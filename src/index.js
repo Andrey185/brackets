f0c378c565a438d9146028dbfd47c426d7fcf512
Andrey <rimmon9@gmail.com>
2023-01-19 00:15:10 +0300

    feat:function check

--- src/index.js
+++ src/index.js
@@ -1,3 +1,73 @@
+// module.exports = function check(str, bracketsConfig) {
+//     let brackets = bracketsConfig.join('').replace(/,/g, '');
+//     let array = [];
+//     let count = 0
+//     for (let bracket of str) {
+//             let bracketsIndex = brackets.indexOf(bracket)
+//             array.push(bracketsIndex)
+//     }
+//     for (let i = 0; i < array.length; i++) {
+//         if (array[i] % 2 == 0 && array[array.length - 1 - i] % 2 !== 0) {
+//         count++
+//         }
+//         else if (array[i] % 2 !== 0 && array[array.length - 1 - i] % 2 == 0) {
+//             count++
+//             }
+//     }
+//     if (array.length-count*2==0) {
+//         return true
+//     } else{return false}
+// }
+
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let list = bracketsConfig.join('').replace(/,/g, '');
+    let array = [];
+    for (let item of str) {
+            let listIndex = list.indexOf(item)
+            if (listIndex % 2 === 0) {
+                
+                if (item === list[listIndex + 1] && array[array.length - 1] === listIndex){
+                    array.pop();
+                } else if (item === list[listIndex + 1] && array[array.length - 1] !== listIndex) {
+                    array.push(listIndex)
+                }
+                else{
+                    array.push(listIndex)
+                }
+            } 
+            else {
+                if (array.pop() !== listIndex-1){
+                    return false;
+                }
+            }
+        }
+        return array.length === 0
 }
+
+
+// function check(str, bracketsConfig) {
+//     let brackets = bracketsConfig.join('').replace(/,/g, '');
+//     let array = [];
+//     let count = 0
+//     for (let bracket of str) {
+//             let bracketsIndex = brackets.indexOf(bracket)
+//         console.log(bracketsIndex);
+//         console.log(brackets[bracketsIndex + 1]);
+//             array.push(bracketsIndex)
+//     }
+//     for (let i = 0; i < array.length; i++) {
+//         if (array[i] % 2 == 0 && array[array.length - 1 - i] % 2 !== 0) {
+//         count++
+//         }
+//         else if (array[i] % 2 !== 0 && array[array.length - 1 - i] % 2 == 0) {
+//             count++
+//             }
+//     }
+//     console.log(array.length);
+//     console.log(count);
+//     if (array.length === count * 2) {
+//         return true
+//     } else{return false}
+// }
+
+// console.log(check('([{}])', [['(', ')'], ['[', ']'], ['{', '}']]));
